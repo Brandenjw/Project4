b1e85b0748a6ced4ed4e89e9a574d112cd03b519
Branden White <Brandenjwhite@gmail.com>
2019-06-10 11:32:25 -0400

    working on components and deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Home from './components/homeComp';
 import Land from './components/landingComp';
-import Bodies from './components/bodyComp';
+// import Bodies from './components/bodyComp';
 import Colors from './components/colorComp';
 import Wheels from './components/wheelComp';
 
-import styled from "styled-components";
+// import styled from "styled-components";
 
-const Title = styled.h1`
-color: black;
-text-align: center;
-font-size: 38px;
-`
+// const Title = styled.h1`
+// color: black;
+// text-align: center;
+// font-size: 38px;
+// `
 
 
 
@@ -26,7 +26,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/landing" component={Land}/>
             <Route exact path="/home" component={Home}/> 
-            <Route exact path="/body/id" component={Bodies}/>
+            {/* <Route exact path="/body/id" component={Bodies}/> */}
             <Route exact path="/color/:id" component={Colors}/>
             <Route exact path="/wheel/:id" component={Wheels}/>
             
