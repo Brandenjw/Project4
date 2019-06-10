7b8c18ec3a09825c180654fb225f23095764a0c4
Branden White <Brandenjwhite@gmail.com>
2019-06-10 08:48:56 -0400

    added routes in server for all models

--- server.js
+++ server.js
@@ -1,21 +1,96 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
+app.use(express.static(__dirname + "/client/build/"));
 
-
-  app.use(express.static(__dirname + '/client/build/'));
-
-  app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/client/build/index.html')
-  })
-
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/client/build/index.html");
+});
 
 app.use(express.json());
-app.get('/', (req,res) => {
-  res.send('Hello world!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+//  BODY MODEL
+//////////////
+
+app.get("/body", (req, res) => {
+  bodyApi.getAllBodies().then(body => {
+    res.send(body);
+  });
+});
+
+// // Posting a new body
+app.post("/body", (req, res) => {
+  bodyApi.createNewBodies(req.body).then(body => {
+    res.send(body);
+  });
+});
+
+// Updating an body
+app.put("/body/:bodyId", (req, res) => {
+  // updateBioById
+  bodyApi
+    .updateBodyById(req.params.bodyId, req.body, { new: true })
+    .then(bio => {
+      res.send(bio);
+    });
+});
+// // Deleting a body
+app.delete("/body/:bodyId", (req, res) => {
+  bodyApi.deleteBodyById(req.params.bodyId).then(() => {
+    res.send(200);
+  });
+});
+
+// COLOR MODEL//
+///////////////////////
+
+app.get("/color", (req, res) => {
+  wheelApi.getAllColors().then(color => {
+    res.send(color);
+  });
+});
+
+// Posting a new COLOR (img)
+app.post("/color", (req, res) => {
+  wheelApi.createNewColor(req.body).then(color => {
+    res.send(Color);
+  });
+});
+
+// Deleting a COLOR Image
+app.delete("/color/:colorId", (req, res) => {
+  wheelApi.deleteColorById(req.params.colorId).then(() => {
+    res.send(200);
+  });
+});
+
+// WHEEL MODEL//
+/////////////////////////
+
+app.get("/wheel", (req, res) => {
+  wheelApi.getAllWheels().then(wheel => {
+    res.send(wheel);
+  });
+});
+
+// Posting a new Wheel (img)
+app.post("/wheel", (req, res) => {
+  wheelApi.createNewWheel(req.body).then(wheel => {
+    res.send(wheel);
+  });
+});
+
+// Deleting a Wheel Image
+app.delete("/wheel/:wheelId", (req, res) => {
+  wheelApi.deleteWheelById(req.params.wheelId).then(() => {
+    res.send(200);
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Magic happening on port " + PORT);
-})
+});
