9ec550ae7a5b90294daf074cd393486dfa130f70
Branden White <Brandenjwhite@gmail.com>
2019-06-10 09:53:49 -0400

    added components for each page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/homeComp';
+import Land from './components/landingComp';
+import Bodies from './components/bodyComp';
+import Colors from './components/colorComp';
+import Wheels from './components/wheelComp';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import styled from "styled-components";
+
+const Title = styled.h1`
+color: black;
+text-align: center;
+font-size: 38px;
+`
+
+
+
+class App extends Component {
+  render () {
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/landing" component={Land}/>
+            <Route exact path="/home" component={Home}/> 
+            <Route exact path="/body/id" component={Bodies}/>
+            <Route exact path="/color/:id" component={Colors}/>
+            <Route exact path="/wheel/:id" component={Wheels}/>
+            
+            <title>Stanced World</title>
+            
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
